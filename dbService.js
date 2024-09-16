37c186dc7dede374ea703dc9d486ebecb2540c73
Natarajan <SNatarajan@ashleyfurniture.com>
2024-09-16 06:25:09 +0530

    MongoDB service + adduser and getuser API

--- dbService.js
+++ dbService.js
@@ -1,1 +1,59 @@
-const mongo = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cabtrack:BlueDiamond2024@cabtrack.346mg.mongodb.net/?retryWrites=true&w=majority&appName=cabtrack";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const dbName = 'cabtrack';
+const collectionName = 'users';
+const database = client.db(dbName);
+const collection = database.collection(collectionName);
+
+exports.connectToDB = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("users").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+};
+
+exports.connectToDB().catch(console.dir);
+
+exports.addQuery = async (inputJson) => {
+  await client.connect();
+  try {
+    const insertResult = await collection.insertOne(JSON.parse(inputJson));
+    await client.close();
+    return {"status":"success"};
+  }
+  catch(err){
+    console.log('Database insert operation failed'+err);
+    return {err};
+  }
+}
+
+
+exports.getQuery = async (inputJson) => {
+  await client.connect();
+  try {
+    const findResult = await collection.findOne(JSON.parse(inputJson));
+    await client.close();
+    console.log("result: "+JSON.stringify(findResult));
+    //return JSON.stringify(findResult);
+    return (findResult);
+  }
+  catch(err){
+    console.log('Database insert operation failed'+err);
+    return err;
+  }
+}
--- index.js
+++ index.js
@@ -1,33 +1,74 @@
 const http = require('http');
+const { addQuery, getQuery } = require('./dbService.js');
 const PORT = 3300;
 
-const httpServer = http.createServer((request, response) => {
+const httpServer = http.createServer(async (request, response) => {
 
+  const url = request.url.toUpperCase();
 
-const url = request.url;
-console.log(url);
+  console.log(url);
 
-let inputJson = '';
+  console.log(url === '/GETUSERS' || url === '/GETUSER' || url === '/ADDUSER');
 
-request.on('data',(data)=>{
- inputJson += data; 
-});
+  if (url === '/GETUSERS' || url === '/GETUSER' || url === '/ADDUSER') {
 
-request.on('end',()=>{
-  console.log(inputJson);
-});
+    if  ((url === '/ADDUSER'  && request.method === 'POST') || 
+         (url === '/GETUSERS' && request.method === 'GET') ||
+         (url === '/GETUSER'  && request.method === 'GET')) {
 
-//let outputJson = {"output":"data", "status":"success"};
-let outputJson = {output:"data", status:"success"};
+      let inputJson = '';
 
-response.write("your input: " + JSON.stringify(outputJson));
-response.end();
+      request.on('data', (data) => {
+        inputJson += data;
+      });
 
+      request.on('end', () => {
+        console.log(inputJson);
+      });
+
+      await inputJson;
+
+      processData(request.method, response, inputJson);
+    } else {
+      response.writeHead('405');   //method not allowed
+      response.end();
+    }
+
+  } else {
+
+    response.writeHead('404');    //resource not found  
+    response.end();
+  }
 
 }
 
 );
 
-httpServer.listen(PORT||3300,(err)=> {
-  console.log("server listening at Port:"+PORT);
-})
+httpServer.listen(PORT || 3300, (err) => {
+  console.log("server listening at Port:" + PORT);
+});
+
+
+async function processData(requestMethod, response, inputJson) {
+  let outputJson = '';
+  console.log("inside function");
+  console.log("inside function" + inputJson);
+  
+  if (requestMethod === 'POST') {
+  outputJson = await addQuery(inputJson);
+  }
+  
+  if (requestMethod === 'GET') {
+    outputJson = await getQuery(inputJson);
+    console.log("output received"+outputJson);
+    }
+
+  //let outputJson = { output: "data", status: "success" };
+
+  response.writeHead('200',{"Content-Type":"application/json"});
+  response.write(JSON.stringify(outputJson));
+  response.end();
+
+}
+
+
